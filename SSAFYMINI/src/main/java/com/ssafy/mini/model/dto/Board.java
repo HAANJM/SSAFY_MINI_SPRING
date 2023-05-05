91c04e3c5101f16442a76cc5ee5dae8bafcae91f
haanwhy <haanwhy@gmail.com>
2023-05-05 18:30:49 +0900

    230505

--- SSAFYMINI/src/main/java/com/ssafy/mini/model/dto/Board.java
+++ SSAFYMINI/src/main/java/com/ssafy/mini/model/dto/Board.java
@@ -1,10 +1,16 @@
 package com.ssafy.mini.model.dto;
 
+import java.util.Date;
+
 public class Board {
 	
+	private int boardNo;
 	private String title;
 	private String content;
 	private String writer;
+	private int likeCount;
 	private int viewCount;
+	private Date createDate;
+	private Date modifyDate;
 
 }
